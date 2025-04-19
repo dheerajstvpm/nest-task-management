059ef3b924f1b5a717350f1de8c3511a567f3283
Dheeraj S <dheerajstvpm@gmail.com>
2025-04-19 22:19:05 +0530

    remove unwanted commented code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-// import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
